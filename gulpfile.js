530cc3be7de269d936452db061b3bf3027767b29
蒲聪德 <pucd@cloudrelation.com>
2017-04-18 13:52:45 +0800

    配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp');
 
 var sass = require('gulp-sass'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename');
 
+//编译sass为 css
 gulp.task('sass',function(){
     return gulp.src('content/sass/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('content/css/'));
 });
+
+//压缩js为min.js
 gulp.task('jsmin', function () {
     gulp.src('src/js/*.js')
         .pipe(uglify({
@@ -16,12 +21,24 @@ gulp.task('jsmin', function () {
             mangle: {except: ['require' ,'exports' ,'module' ,'$']}//排除混淆关键字
             //preserveComments: 'all' //保留所有注释
         }))
+        .pipe(rename({suffix:'.min'}))
         .pipe(gulp.dest('dist/js'));
 });
 
+//压缩css
+gulp.task('cssmin',['sass'],function () {
+    gulp.src('content/css/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix:'.min'}))
+        .pipe(gulp.dest('dist/css'));
+});
 
+//监视任务的变化
 gulp.task('watch',function() {
     gulp.watch('src/js/*.js',['jsmin']);
   //  gulp.wath('src/html/*.html',['html']);
    // gulp.wath('src/lib/*.js',['js']);
 });
+
+//默认的任务
+gulp.task('default',['sass','jsmin','cssmin']);
